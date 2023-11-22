6b45ac5edd357df1620bf26462765ee33506349f
Arnaud <bekaman100@gmail.com>
2023-11-22 16:42:59 +0100

    dernière mise à jours

--- assets/js/simulateur.js
+++ assets/js/simulateur.js
@@ -4,32 +4,64 @@ const inputTitle = document.getElementById("inputTitle");
 const totalTitlePixels = document.getElementById("titlePixelsRemind");
 const inputMd = document.getElementById("inputMd");
 const totalMdPixels = document.getElementById("mdPixelsRemind");
+const inputUrl = document.getElementById("inputUrl");
+const totalUrlPixels = document.getElementById("urlPixelsRemind");
+
+
+
 
 // Création d'un élément canvas unique pour la mesure du texte
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
-ctx.font = '14px arial';
+
 
 // Fonction pour calculer la largeur du texte en pixels
-function calculateTextWidth(text) {
+function calculateTextWidth(text, tag) {
+    if (tag === "title") {
+        ctx.font = '20px arial';
+    } else if (tag === "meta") {
+        ctx.font = '14px arial';
+    } else if (tag === "url") {
+        ctx.font = '16px arial';
+    }
+
     return ctx.measureText(text).width;
 }
 
 // Fonction générique pour gérer les événements d'entrée
-function handleInput(inputElement, totalPixelsElement, maxPixels) {
+function handleInput(inputElement, totalPixelsElement, maxPixels, tag) {
     const characters = inputElement.value;
-    const width = calculateTextWidth(characters);
+    const width = calculateTextWidth(characters, tag);
     const remainingPixels = maxPixels - width;
 
     totalPixelsElement.classList.toggle("text-danger", remainingPixels < 0);
     totalPixelsElement.classList.toggle("text-success", remainingPixels >= 0);
 
     totalPixelsElement.innerText = Math.ceil(remainingPixels);
+
+    //On recupère les éléments du tableau de données
+    const tableData = document.getElementsByClassName(tag + "-data");
+
+    tableData[0].innerText = inputElement.value.length; 
+     
+    while (remainingPixels > 0) {
+        tableData[1].innerText = (tableData[0].innerText * 1);
+        remainingPixels -= width
+    
+        if (remainingPixels < 0) {
+            break; // Sortir de la boucle while
+        }
+    }
+
+    // On fait la different des caractères moins les caractère affichés pour determiner les caractères tronqués
+    tableData[2].innerText = (tableData[0].innerText * 1) - (tableData[1].innerText * 1)
+  
 }
 
 // Ecouteurs d'événements pour les champs de saisie
-inputTitle.addEventListener('input', () => handleInput(inputTitle, totalTitlePixels, 500));
-inputMd.addEventListener('input', () => handleInput(inputMd, totalMdPixels, 930));
+inputTitle.addEventListener('input', () => handleInput(inputTitle, totalTitlePixels, 600, "title"));
+inputMd.addEventListener('input', () => handleInput(inputMd, totalMdPixels, 920, "meta"));
+inputUrl.addEventListener('input', () => handleInput(inputUrl, totalUrlPixels, 1280, "url"));
 
   
 
